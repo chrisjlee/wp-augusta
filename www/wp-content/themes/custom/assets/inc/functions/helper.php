42c327c359d9df6f18e6f16ae872c2b0ffb3913f
Chris Lee <chrisjl@gmail.com>
2011-07-29 07:41:50 -0500

    Integrate Doctype setup from thematic theme - stole it.

--- www/wp-content/themes/custom/assets/inc/functions/helper.php
+++ www/wp-content/themes/custom/assets/inc/functions/helper.php
@@ -40,22 +40,47 @@ add_filter('body_class','augusta_browser_body_class');
  * allow convert a theme to html5
  * 
  * By default augusta is a html theme.
+ * 
+ * $content - filterable variable
  *  
  */
-if (!function_exists( 'custom_augusta_doctype_setup' ) ) :
+if (!function_exists( 'custom_doctype_setup' ) ) :
 function augusta_doctype_setup() {
-  echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<!--[if IE 6]><html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" class="ie6 no-js"><![endif]-->
+  $content = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">' . "\n";
+$content .= '<!--[if IE 6]><html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" class="ie6 no-js"><![endif]-->
 <!--[if lt IE 7]><html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" class="ie ie6 lte9 lte8 lte7 no-js"><![endif]-->
 <!--[if IE 7]><html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" class="ie ie7 lte9 lte8 lte7 no-js"> <![endif]-->
 <!--[if IE 8]><html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" class="ie ie8 lte9 lte8 no-js"><![endif]-->
 <!--[if IE 9]><html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" class="ie ie9 lte9 no-js"><![endif]-->
 <!--[if gt IE 9]><html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" class="gtie9 ie9 no-js"><![endif]-->
+<!--[if (gte IE 9)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!--><html class="no-js" manifest="default.appcache?v=1"
 <!--[if !IE]><html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" class="not-ie no-js"><!--<![endif]-->
 <head profile="http://gmpg.org/xfn/11">';
+  echo apply_filters('augusta_doctype_setup', $content);
 }
 add_action('augusta_doctype', 'augusta_doctype_setup');
 endif;
+
+/**
+ * Child Theme Function: 
+ * Copy the following below to convert 
+ * a child them to HTML5
+
+// Change the doctype
+function childtheme_doctype_setup($content) {
+  $content = '<!DOCTYPE html>';
+  $content .= "\n";
+  $content .= '<!--[if IEMobile 7 ]><html class="no-js iem7" manifest="default.appcache?v=1"><![endif]-->
+  <!--[if lt IE 7 ]><html class="no-js ie6" lang="en"><![endif]-->
+  <!--[if IE 7 ]><html class="no-js ie7" lang="en"><![endif]-->
+  <!--[if IE 8 ]><html class="no-js ie8" lang="en"><![endif]-->
+  <!--[if (gte IE 9)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!--><html class="no-js" manifest="default.appcache?v=1"';
+  return $content;
+}
+   
+ *
+ */
+
 
 /*
  * Implements augusta_title
@@ -102,7 +127,7 @@ function augusta_title_setup() {
   if ($page_title != '') $page_title .= ' '.$separator.' ';
  $page_title .= $blog_name;
   
-  echo $page_title;
+  echo apply_filters('augusta_title_setup',$page_title);
 }
 add_action('augusta_title', 'augusta_title_setup');
 endif;
